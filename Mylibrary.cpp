7e7d5fa75bb69ebd9dee6be36e2a5c9d3fb3eab4
NURFARHANAH03 <126445460+NURFARHANAH03@users.noreply.github.com>
2024-07-31 00:42:26 +0800

    Add files via upload

---
+++ Mylibrary.cpp
@@ -0,0 +1,20 @@
+// MyLibrary.cpp
+#include "Mylibrary.h"
+
+MyLibrary::MyLibrary() {}
+
+void MyLibrary::insertEntry(const std::string& id, const std::string& password) {
+    storedIdPasswordMap[id] = password;
+}
+
+bool MyLibrary::checkId(std::string& id) {
+    return storedIdPasswordMap.find(id) != storedIdPasswordMap.end();
+}
+
+bool MyLibrary::checkPassword(std::string& id, std::string& password) {
+    std::map<std::string, std::string>::iterator it = storedIdPasswordMap.find(id);
+    if (it != storedIdPasswordMap.end()) {
+        return it->second == password;
+    }
+    return false;
+}
